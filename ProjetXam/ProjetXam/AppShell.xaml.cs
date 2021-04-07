8113f9011e17153c777a3ede654aa0c92002f402
Marc Tarrade <marctarrade@gmail.com>
2021-04-07 13:22:40 +0200

    ajout navigation

---
+++ ProjetXam/ProjetXam/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ProjetXam
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
