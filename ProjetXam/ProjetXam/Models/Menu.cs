0f21b335e87f362f100abc1229d1dd24c16fec1a
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-04-11 15:21:23 +0200

    ajout liste menus

---
+++ ProjetXam/ProjetXam/Models/Menu.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetXam.Models
+{
+    public class Menu
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+    }
+}
---
+++ ProjetXam/ProjetXam/ViewModels/MenusViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using ProjetXam.Models;
+using ProjetXam.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetXam.ViewModels
+{
+    class MenusViewModel : BaseViewModel
+    {
+        ObservableCollection<Menu> menus = new ObservableCollection<Menu>();
+        Menu menuSelected;
+
+        public Menu MenuSelected
+        {
+            get { return menuSelected; }
+            set { SetProperty(ref menuSelected, value); }
+        }
+
+        public ObservableCollection<Menu> Menus
+        {
+            get { return menus; }
+            set { SetProperty(ref menus, value); }
+        }
+
+        public MenusViewModel()
+        {
+            _ = getMenusApi();
+        }
+
+        private async Task getMenusApi()
+        {
+            var client = HttpService.getInstance();
+            var result = await client.GetAsync("https://raw.githubusercontent.com/Guillaume2304/lp-dam-x-btvv/main/menu2.json");
+            var serializedResponse = await result.Content.ReadAsStringAsync();
+            Menus = JsonConvert.DeserializeObject<ObservableCollection<Menu>>(serializedResponse);
+        }
+
+        
+    }
+}
